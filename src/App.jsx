4d66b67ebbcceeb6c78f9f3ded75a9e3301566e5
axwvvvvv <galiyevamirbek84@gmail.com>
2024-11-01 16:54:50 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Card from './components/Card/Card'
+import Wrapper from './components/Wrapper/Wrapper'
+
+const App = () => {
+  return (
+    <>
+    <Wrapper/>
+    </>
+  )
+}
+
+export default App
